8f58faaadb14809676634bbd42e97dc5c69d7a0f
ashokp_700036 <poolaaashok386@gmail.com>
2023-02-13 17:16:23 +0530

    wavekartTesting

--- src/Testcase/wavecartTest.java
+++ src/Testcase/wavecartTest.java
@@ -3,56 +3,52 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.awt.*;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-public class wavecartTest {
+public class WaveCartTest {
     WebDriver driver;
+
     WebDriverWait wait;
 
     @BeforeMethod
-    public void setUp()  {
+    public void setUp() {
         System.setProperty("Webdriver.chrome.driver", "G:\\chromedriver\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-        implicitlyWait(20);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        implicitlyWait();
         driver.get("https://www.wavemakeronline.com/run-0dnqs4vt0q/WaveKart_master/#/Main");
     }
 
     //@dropdown
-    public void dropdown1(String index, String value)  {
+    public void dropDown1(String value) {
         Select dropdown1 = new Select(driver.findElement(By.xpath(String.format("//select[@aria-expanded='false']"))));
         dropdown1.selectByVisibleText(value);
     }
 
 
     @Test
-    public void product() throws InterruptedException {
+    public void product() {
         verify("https://www.wavemakeronline.com/run-0dnqs4vt0q/WaveKart_master/#/Main");
-       WaveClick("(//label[text()='Moto G (Black, with 8 GB)'])");
-       //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//label[text()='Moto G (Black, with 8 GB)'])")));
-        WaveClick("//button[@class='btn app-button btn-warning']");
-        explicitlyWait("//button[@class='btn app-button btn-warning']");
-        waveMethodSendKeys("//input[@name='j_username']", "user");
-        waveMethodSendKeys2("//input[@name='j_password']", "user");
-        WaveClick("//button[@caption='Sign in']");
-        dropdown1("0", "3");
-        WaveClick("//button[@caption='Place Order']");
-        WaveClick("//button[@class='btn app-button btn-primary']");
-        WaveClick("//button[@name='nextBtn_wizard_payment']");
-        cashOnDelivery("//label[@title='Cash On Delivery(COD)']");
-        WaveClick("//button[@class='btn app-button btn-primary']");
-        WaveClick("//button[@class='btn app-button btn-success']");
+        waveClick(By.xpath("(//label[text()='Moto G (Black, with 8 GB)'])"));
+        waveClick(By.xpath("//button[@class='btn app-button btn-warning']"));
+        waveMethodSendKeys(By.xpath("//input[@name='j_username']"), "user");
+        waveMethodSendKeys(By.xpath("//input[@name='j_password']"), "user");
+        waveClick(By.xpath("//button[@caption='Sign in']"));
+        dropDown1("3");
+        waveClick(By.xpath("//button[@caption='Place Order']"));
+        waveClick(By.xpath("//button[@class='btn app-button btn-primary']"));
+        waveClick(By.xpath("//button[@name='nextBtn_wizard_payment']"));
+        cashOnDelivery(By.xpath("//label[@title='Cash On Delivery(COD)']"));
+        waveClick(By.xpath("//button[@class='btn app-button btn-primary']"));
+        waveClick(By.xpath("//button[@class='btn app-button btn-success']"));
 
     }
 
@@ -61,24 +57,20 @@ public class wavecartTest {
         driver.quit();
     }
 
-    public void WaveClick(String Expression) {
-        driver.findElement(By.xpath(Expression)).click();
+    public void waveClick(By locator) {
+        driver.findElement(locator).click();
     }
 
-    public void waveMethodSendKeys(String Expression, String user) {
-        driver.findElement(By.xpath(Expression)).sendKeys(user);
+    public void waveMethodSendKeys(By Expression, String user) {
+        driver.findElement(Expression).sendKeys(user);
     }
 
-    public void waveMethodSendKeys2(String Expression, String user) {
-        driver.findElement(By.xpath(Expression)).sendKeys(user);
+    public WebElement cashOnDelivery(By Expression) {
+
+        return driver.findElement(Expression);
     }
 
-    public WebElement cashOnDelivery(String Expression) {
-
-        return driver.findElement(By.xpath(Expression));
-    }
-
-    public void implicitlyWait(int time) {
+    public void implicitlyWait() {
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
@@ -87,12 +79,6 @@ public class wavecartTest {
         String ExpectedUrl = "https://www.wavemakeronline.com/run-0dnqs4vt0q/WaveKart_master/#/Main";
         Assert.assertEquals(actualUrl, ExpectedUrl);
         System.out.println(ExpectedUrl);
-    }
-
-
-    public void explicitlyWait(String Expression ) {
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(Expression)));
     }
 }
 
